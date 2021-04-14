3bf48a1a4fd4db072dba76b8ac7f4e75aa00f962
Sam Slotemaker <sam.slotemaker@ziggo.nl>
2021-04-14 16:10:56 +0200

    added buttons to choose depart time

--- prototype-2/script.js
+++ prototype-2/script.js
@@ -1,6 +1,9 @@
 const API_KEY = 'a17a85a0327a026d6ccffda6769b4782'
 const CITY = 'heiloo'
 const API_URL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${API_KEY}`
+const cursor = document.querySelector('.cursor')
+console.log(cursor);
+
 
 var rainAudio = new Audio('./sounds/rain1.mp3')
 var dryAudio = new Audio('./sounds/dry1.mp3')
@@ -21,3 +24,11 @@ async function getData(url) {
 window.onclick = function () {
     dryAudio.play()
 }
+
+
+window.addEventListener('mousemove', (e) => {
+    let y = e.pageY - (cursor.offsetHeight / 2)
+    let x = e.pageX - (cursor.offsetWidth / 2)
+    cursor.style.top = y + 'px'
+    cursor.style.left = x + 'px'
+})
