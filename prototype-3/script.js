b5ba2981a651b7a2c07af30016ca3a5b204b4b8c
Sam Slotemaker <sam.slotemaker@ziggo.nl>
2021-04-22 16:45:53 +0200

    per minute and working with real data in Amsterdam

--- prototype-3/script.js
+++ prototype-3/script.js
@@ -1,31 +1,98 @@
 const API_KEY = 'a17a85a0327a026d6ccffda6769b4782'
 const CITY = 'heiloo'
-const API_URL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${API_KEY}`
+const LAT = '52.36508986732812'
+const LON = '4.866084348096675'
+const API_URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${LAT}&lon=${LON}&appid=${API_KEY}`
+
+const testArray = [{ dt: 1619102580, precipitation: 0 }
+    , { dt: 1619102640, precipitation: 0 }
+    , { dt: 1619102700, precipitation: 0 }
+    , { dt: 1619102760, precipitation: 0 }
+    , { dt: 1619102820, precipitation: 0 }
+    , { dt: 1619102880, precipitation: 0 }
+    , { dt: 1619102940, precipitation: 0 }
+    , { dt: 1619103000, precipitation: 0 }
+    , { dt: 1619103060, precipitation: 0 }
+    , { dt: 1619103120, precipitation: 0 }
+    , { dt: 1619103180, precipitation: 0 }
+    , { dt: 1619103240, precipitation: 0 }
+    , { dt: 1619103300, precipitation: 0 }
+    , { dt: 1619103360, precipitation: 0 }
+    , { dt: 1619103420, precipitation: 0 }
+    , { dt: 1619103480, precipitation: 0 }
+    , { dt: 1619103540, precipitation: 0 }
+    , { dt: 1619103600, precipitation: 0 }
+    , { dt: 1619103660, precipitation: 0 }
+    , { dt: 1619103720, precipitation: 0 }
+    , { dt: 1619103780, precipitation: 0 }
+    , { dt: 1619103840, precipitation: 0 }
+    , { dt: 1619103900, precipitation: 0 }
+    , { dt: 1619103960, precipitation: 0 }
+    , { dt: 1619102580, precipitation: 0 }
+    , { dt: 1619102640, precipitation: 0 }
+    , { dt: 1619102700, precipitation: 0 }
+    , { dt: 1619102760, precipitation: 0 }
+    , { dt: 1619102820, precipitation: 0 }
+    , { dt: 1619102880, precipitation: 0 }
+    , { dt: 1619102940, precipitation: 0 }
+    , { dt: 1619103000, precipitation: 0 }
+    , { dt: 1619103060, precipitation: 0 }
+    , { dt: 1619103120, precipitation: 0 }
+    , { dt: 1619103180, precipitation: 0 }
+    , { dt: 1619103240, precipitation: 0 }
+    , { dt: 1619103300, precipitation: 0 }
+    , { dt: 1619103360, precipitation: 0 }
+    , { dt: 1619103420, precipitation: 0 }
+    , { dt: 1619103480, precipitation: 0 }
+    , { dt: 1619103540, precipitation: 0 }
+    , { dt: 1619103600, precipitation: 0 }
+    , { dt: 1619103660, precipitation: 0 }
+    , { dt: 1619103720, precipitation: 0 }
+    , { dt: 1619103780, precipitation: 1 }
+    , { dt: 1619103840, precipitation: 0 }
+    , { dt: 1619103900, precipitation: 0 }
+    , { dt: 1619103960, precipitation: 0 }
+    , { dt: 1619103240, precipitation: 0 }
+    , { dt: 1619103300, precipitation: 0 }
+    , { dt: 1619103360, precipitation: 0 }
+    , { dt: 1619103420, precipitation: 0 }
+    , { dt: 1619103480, precipitation: 0 }
+    , { dt: 1619103540, precipitation: 0 }
+    , { dt: 1619103600, precipitation: 0 }
+    , { dt: 1619103660, precipitation: 0 }
+    , { dt: 1619103720, precipitation: 0 }
+    , { dt: 1619103780, precipitation: 0 }
+    , { dt: 1619103840, precipitation: 0 }
+    , { dt: 1619103900, precipitation: 0 }
+    , { dt: 1619103960, precipitation: 0 }]
+
 const timeElement = document.querySelector('.tijd p')
 var msg = new SpeechSynthesisUtterance();
 msg.lang = 'nl'
-
-
 
 var rainAudio = new Audio('./sounds/rain1.mp3')
 var dryAudio = new Audio('./sounds/dry1.mp3')
 dryAudio.volume = 0.5
 rainAudio.volume = 0.5
 
+let dryMinutes = 61;
+getData(API_URL).then(data => {
+    const minutesArray = data.minutely
+    dryMinutes = findMinutes(testArray)
+    console.log(dryMinutes)
+})
 
-
-async function getData(url) {
-    const response = await fetch(url)
-    const data = await response.json()
-    return data
+//returns how many minutes it will stay dry
+function findMinutes(array) {
+    //return array index when there will be rain
+    let dryMinutes = 61
+    array.forEach((item, index) => {
+        if (item.precipitation > 0) {
+            dryMinutes = index
+        }
+    })
+    return dryMinutes;
 }
-
-// getData(API_URL).then(data => {
-//     console.log(data)
-// })
-// window.onclick = function () {
-//     dryAudio.play()
-// }
 
 
 let time = 40
@@ -35,16 +102,17 @@ changeElement(time)
 function handleScroll(e) {
     if (e.deltaY > 0) {
         if (time > 0) {
-            time -= 10;
+            time -= 1;
         }
         speak(time)
         changeElement(time)
         changeWeather(time)
     } else {
-        time += 10;
+        if (time < 60) {
+            time += 1;
+        }
         speak(time)
         changeElement(time)
-
         changeWeather(time)
     }
 }
@@ -72,7 +140,7 @@ function logKey(e) {
 }
 
 function changeWeather(time) {
-    if (time > 50) {
+    if (time > dryMinutes) {
         document.body.classList.add('rain')
         rainAudio.play()
         dryAudio.pause();
@@ -113,5 +181,10 @@ function speak(textMessage) {
     window.speechSynthesis.speak(msg);
 }
 
-document.addEventListener('keydown', logKey);
 document.addEventListener('wheel', handleScroll)
+
+async function getData(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
